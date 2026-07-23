4bd90960378d36d47e3b2c77524334839e12b80f
Rincewind610 <volker@website-fuer-dich.de>
2026-07-24 00:09:47 +0200

    0.3.3

--- praege-vorbereitung.js
+++ praege-vorbereitung.js
@@ -2,7 +2,7 @@
 =======================================
 DS Helper
 Name: Prägevorbereitung
-Version: 0.3.2
+Version: 0.3.3
 Kategorie: Produktion
 Autor: Rincewind610
 
@@ -18,7 +18,7 @@ Status: Entwicklung / Simulation
 (function () {
     'use strict';
 
-    const VERSION = '0.3.2';
+    const VERSION = '0.3.3';
 
     const COIN_VILLAGE = {
         x: 538,
