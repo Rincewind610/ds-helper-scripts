fd64a15e6612954172f2fd89c4ef350bfd83d3d8
Rincewind610 <volker@website-fuer-dich.de>
2026-07-29 22:37:45 +0200

    Sender-Dorf-ID für die Marktplatz-URL 2

---
+++ tore --source=533f016 -- praege-vorbereitung.js
@@ -0,0 +1,64 @@
+[33m55594c6[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Sender-Dorf-ID für die Marktplatz-URL
+[33m533f016[m Transportliste wurde kopiert.
+[33m5a7702c[m Geplante Dorftransporte
+[33mcd8653f[m 0.6.3.1
+[33mc899ae0[m feat: Senderstatistik für Dorftransporte 3
+[33md32bf84[m feat: Senderstatistik für Dorftransporte 2
+[33mb5bea27[m feat: Senderstatistik für Dorftransporte
+[33mbb3a823[m 0.6.2.1
+[33md19f018[m feat: Dorftransporte auf aktuell freie Händler begrenzen
+[33m4733689[m feat: Transportgröße gegen Händlerkapazität prüfen
+[33mfe1cff9[m refactor: Gruppenflüsse aus tatsächlichen Sender-Pools berechnen
+[33mef3e42a[m feat: alle Gruppenflüsse auf Dorfebene simulieren
+[33m66d6b6f[m 0.5.4
+[33m1983098[m restore: stabilen Stand 0.5.3 als 0.5.3.1 wiederhergestellt
+[33m568db21[m refactor: Dorftransporte für beliebigen Gruppenfluss vorbereiten
+[33mc822293[m feat: mehrere Sender für ersten Gruppenfluss simulieren
+[33ma592d5b[m feat: Senderbestand nach jedem Transport reduzieren
+[33mdfec8cc[m 0.5.1.2
+[33m6a4ade2[m 0.5.1.1
+[33ma17ab24[m 0.5.1
+[33mbd12357[m feat: ersten Dorftransport simulieren
+[33me01530e[m feat: Versandkapazität der Senderdörfer je Gruppe zusammenfassen
+[33mb57652e[m feat: Sperrbestand und Händlerkapazität in Dorf-Pools ergänzen 2
+[33m8c51cba[m feat: Sperrbestand und Händlerkapazität in Dorf-Pools ergänzen
+[33m4ab1c5f[m feat: Sperrbestand für Senderdörfer einführen
+[33m77be3e7[m revert: auf stabilen Stand 0.4.8 zurückgesetzt
+[33md8dbb39[m feat: Globalen Sperrbestand für Senderdörfer einführen 2
+[33mbc8a7ee[m feat: Globalen Sperrbestand für Senderdörfer einführen
+[33mc86861d[m feat: Dorf-Pools für Empfänger und Lieferanten vorbereiten 2
+[33ma0817f6[m feat: Dorf-Pools für Empfänger und Lieferanten vorbereiten
+[33mb2cd891[m feat: Gruppenflüsse und Restbedarf im Popup anzeigen
+[33m54fa7e2[m feat: Ressourcenflüsse zwischen Entfernungsgruppen berechnen
+[33m4cf822d[m feat: Ressourcensaldo je Entfernungsgruppe berechnen
+[33mdda7465[m feat: Individuelle Zielfüllstände je Entfernungsgruppe
+[33md38aac9[m feat: Entfernungsgruppen bis Gruppe 8 erweitern
+[33m04d673e[m doppelte Gruppenüberschrift ist damit entfernt
+[33mc25c4e1[m feat: Gruppenbilanz für Entfernungsgruppen berechnen
+[33m10e1270[m feat: Dörfer nach Entfernung zum Münzdorf gruppieren
+[33m5f627a2[m Distanz
+[33m4e14469[m feat: Dörfer intern als Sender oder Empfänger klassifizieren
+[33m4feb846[m Begriff Rolle neu
+[33mfbbd0dd[m Begriff Rolle
+[33m8e9abed[m feat: Ressourcenüberschuss je Dorf berechnen
+[33m24bd4bd[m feat: Sollbestand und Ressourcenbedarf berechnen
+[33m9a09f26[m fix: Dorfzeilen über quickedit-vn eindeutig erkennen
+[33m2e83020[m fix: Dorfzeilen über quickedit-vn eindeutig erkennen
+[33ma0244da[m [DS Helper | Lesefehler]
+[33m25c48af[m [DS Helper | Lesefehler]
+[33m738b11a[m lesefehler behoben
+[33m26c005f[m wieder 0.3.3.
+[33mdc2cc51[m fix: Dorfzeilen anhand eindeutiger Koordinate erkennen
+[33m8d09602[m [DS Helper | Fehlende Koordinaten]
+[33m9aad5d1[m fix: Nur echte Dorfzeilen der Produktionsübersicht einlesen
+[33m381937b[m readVillages
+[33m48d4044[m debug: fehlerhaft eingelesene Dörfer im Popup anzeigen
+[33m4bd9096[m 0.3.3
+[33m3f65599[m 032
+[33m1441a1a[m console log angepasst
+[33m9e09f87[m 0.3.1
+[33m4ad2956[m fix: Lagerkapazität aus Produktionsübersicht korrekt einlesen
+[33m1d50acc[m fix: Lagerkapazität aus Produktionsübersicht korrekt einlesen
+[33m5310c16[m feat: Produktionsdaten und Händler auslesen
+[33m2d893c2[m feat: Grundgerüst für Prägevorbereitung erstellt
+[33m74073eb[m feat: Projekt Prägevorbereitung angelegt
